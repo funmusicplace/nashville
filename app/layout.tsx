e8393962a9dce17fdb8422b820db832cc3db3c99
Simon <simon@mirlo.space>
2025-06-25 17:04:38 -0400

    fix: attempt to do some head things

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,32 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <title>Get us to Nashville</title>
+        <meta property="og:title" content="Get us to Nashville" />
+        <meta
+          property="og:description"
+          content="A fundraiser to get to Nashville"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="https://mirlo.space/favicon-32x32.png"
+        ></link>
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="https://mirlo.space/favicon-16x16.png"
+        ></link>
+        <meta name="twitter:card" content="summary"></meta>
+        <meta property="og:site_name" content="A Fundraiser on Mirlo"></meta>
+        <meta
+          property="og:image"
+          content="https://mirlo.space/android-chrome-512x512.png"
+        ></meta>
+      </head>
       <body>{children}</body>
     </html>
   );
