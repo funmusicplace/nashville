04288083ed1c3020867d42fbb29ad49632903107
Simon <simon@mirlo.space>
2025-06-25 15:42:41 -0400

    fix: try a new host

--- app/lib/api.ts
+++ app/lib/api.ts
@@ -1,7 +1,7 @@
 import APIInstance from "@mirlo/mirlo-api-client";
 
 const API_HOST = process.env.API_HOST || "http://localhost:3000";
-
+console.log("API_HOST:", API_HOST);
 const api = APIInstance(API_HOST, "");
 
 export default api;
